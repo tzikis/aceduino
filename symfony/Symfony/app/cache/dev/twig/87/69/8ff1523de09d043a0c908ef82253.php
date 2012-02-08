d35373e953f9ed2362c80946a5a7b27ec5be1913
Vasileios Georgitzikis <billgeo13@gmail.com>
2012-02-09 01:39:35 +0200

    added User bundle

---
+++ symfony/Symfony/app/cache/dev/twig/87/69/8ff1523de09d043a0c908ef82253.php
@@ -0,0 +1,18 @@
+<?php
+
+/* AceEditorBundle:Default:index.html.twig */
+class __TwigTemplate_87698ff1523de09d043a0c908ef82253 extends Twig_Template
+{
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "AceEditorBundle:Default:index.html.twig";
+    }
+
+}
